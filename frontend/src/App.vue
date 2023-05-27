72c1265ff27fc112e26e0450b15914e8f17c1347
saboten <hellofromeizo@gmail.com>
2023-05-28 00:08:20 +0900

    mod css2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,18 +5,27 @@
       <li v-for="product in products" :key="product.id">
         <h3>{{ product.name }}</h3>
         <p>{{ product.price }}円</p>
-        <div ref="qrcode"></div>
         <button @click="handleProductClick(product)">アフィリエイトリンク</button>
+        <div v-if="selectedProduct && selectedProduct.id === product.id">
+          <div v-qrcode="affiliateLink" class="qrcode"></div>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import VueQrcode from 'vue-qrcode-directive';
+
 export default {
+  directives: {
+    'qrcode': VueQrcode
+  },
   data() {
     return {
       products: [],
+      selectedProduct: null,
+      affiliateLink: '',
     };
   },
   mounted() {
@@ -35,83 +44,16 @@ export default {
     },
     handleProductClick(product) {
       // ここで選択した商品をアフィリエイトリンクと共に処理するコードを追加する
-      console.log('選択した商品:', product);
-
-      // アフィリエイトリンクをQRコードのAPIに渡して画像URLを取得する
-      const affiliateLink = `https://example.com/affiliate?id=${product.id}`;
-      const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(
-        affiliateLink
-      )}`;
-
-      // QRコードを表示する要素に画像タグを挿入する
-      const qrcodeContainer = this.$refs.qrcode;
-      qrcodeContainer.innerHTML = `<img src="${qrCodeUrl}" alt="QR Code">`;
+      this.selectedProduct = product;
+      this.affiliateLink = 'https://affiliate-link.com/' + product.id; // ここに実際のアフィリエイトリンクを生成するロジックを追加する
     },
   },
 };
 </script>
 
-<style scoped>
-.container {
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 20px;
-}
-
-.product-item {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 20px;
-}
-
-@media screen and (min-width: 768px) {
-  .product-item {
-    flex-direction: row;
-    justify-content: space-between;
-  }
-}
-
-/* 修正済みスタイル */
-.container {
-  background-color: #1f2937;
-  color: #ffffff;
-  border-radius: 10px;
-  padding: 30px;
-}
-
-.container h1 {
-  font-size: 32px;
-  margin-bottom: 30px;
-  color: #ffffff; /* テキストカラーを再度指定 */
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  background-color: #ffffff;
-  padding: 20px;
-  border-radius: 10px;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  transition: transform 0.3s ease;
-}
-
-li:hover {
-  transform: translateY(-5px);
-}
-
-h3 {
-  font-size: 24px;
-  color: #1f2937;
-  margin-bottom: 10px;
-}
-
-p {
-  font-size: 18px;
-  color: #4b5563;
-  margin: 0;
+<style>
+.qrcode {
+  width: 100px;
+  height: 100px;
 }
 </style>
